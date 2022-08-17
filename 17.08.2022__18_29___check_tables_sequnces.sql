598058fb3869d56ab949175dd4cae4410c810598
j_dev <j.developer.days@gmail.com>
2022-08-17 18:30:35 +0300

    create and drop table create sequnces

---
+++ 17.08.2022__18_29___check_tables_sequnces.sql
@@ -0,0 +1,21 @@
+select *
+from information_schema."tables" t
+--where table_name = 't_phone_number_types' or table_name = 't_countries';
+where table_name in('t_phone_number_types', 't_countries') ;
+
+select *
+from information_schema."columns" c
+where table_name = 't_phone_number_types';
+
+
+------
+select *
+from information_schema."sequences" s
+where sequence_name in('phone_number_types_seq', 'countries_seq');
+
+
+select *
+from phone_book.t_phone_number_types;
+
+select *
+from phone_book.t_countries;2
